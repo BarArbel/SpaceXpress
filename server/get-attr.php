f1a39c5cf09f79cf512da12d4cab28c8a0c198ae
MiriNu <miryamnudelman@gmail.com>
2018-07-13 16:02:51 +0300

    blup

--- server/get-attr.php
+++ server/get-attr.php
@@ -13,31 +13,6 @@
     	$rows[] = $r;
 	}
 	echo json_encode($rows);
-
-/*	include('db.php');
-	
-	$query1 = "SELECT * FROM tbl_test order by title desc";
-	$result = mysqli_query($connection, $query1);
-	if(!$result) {
-		die("DB query failed.");
-	}
-	
-	if(isset($_POST['ttl'])) {
-		$ttle = mysqli_real_escape_string($connection, $_POST['ttl']);
-		$txt = mysqli_real_escape_string($connection, $_POST['desc']);
-		
-		$query2 = "insert into tbl_test(title,txt) values ('$ttle','$txt')";
-		$result = mysqli_query($connection, $query2);
-		
-		$query2 = "SELECT * FROM tbl_test order by title desc";
-		$result = mysqli_query($connection, $query2);
-	}
-	
-	echo "<ul>";
-	while($row = mysqli_fetch_assoc($result)) {
-		echo "<li><h2>" . $row["title"] . "</h2><h3>" . $row["txt"] . "</h3></li>";
-	}
-	echo "</ul>";*/
 	
 	mysqli_free_result($result);
 	
--- server/set-destination.php
+++ server/set-destination.php
@@ -1,40 +1,76 @@
 <?php
     include ('db.php');
-    $get_latest_dest = "SELECT MAX(dest_id) AS max_dest FROM tbl_205_destination";
+
+    //getting the pic of the planet according to the name
+    function picNum($planet) {
+        $ascii = ord(strtolower(($planet[0])));
+        return abs(($ascii-96))%10;
+    }
+
+    function fixDateFormat ($date) {
+        return date_format(new DateTime($date),"Y-m-d");
+    }
+
     $get_latest_trip = "SELECT MAX(trip_id) AS max_trip FROM tbl_205_trip";
-
     $trip_id = mysqli_fetch_array(mysqli_query($connection, $get_latest_trip))["max_trip"]+1;
-    echo $trip_id;
-    $dest_id1 = mysqli_fetch_array(mysqli_query($connection, $get_latest_dest))["max_dest"]+1;
-
-    $dest_id2 = $dest_id1+1;
-    $dest_id3 = $dest_id2+1;
 
     $planet_name1 = $_GET["planet1"];
-    $planet_name2 = $_GET["planet2"];
-    $planet_name3 = $_GET["planet3"];
+    $pic1 = "images/planet" . picNum($planet_name1) . ".png";
 
-    $strt1 = $_GET["start1"];
-    $strt2 = $_GET["start2"];
-    $strt3 = $_GET["start3"];
+    if (!empty($_GET['planet2'])) {
+        $planet_name2 = $_GET["planet2"];
+        $pic2 = "images/planet" . picNum($planet_name2) . ".png";
+    }
 
-    $end1 = $_GET["end1"];
-    $end2 = $_GET["end2"];
-    $end3 = $_GET["end3"];
+    if (!empty($_GET['planet3'])) {
+        $planet_name3 = $_GET["planet3"];
+        $pic3 = "images/planet" . picNum($planet_name3) . ".png";
+    }
 
-$qSetDest = "INSERT INTO tbl_205_destination (dest_id, user_id, trip_id, planet_name, arrival_date, depart_date, picture_url) VALUES ($dest_id1,4,$trip_id,'$planet_name1',$strt1,$end1,'pic')";
+    $strt1 = fixDateFormat($_GET["start1"]);
+    if (!empty($_GET['start2'])) $strt2 = fixDateFormat($_GET["start2"]);
+    if (!empty($_GET['start3'])) $strt3 = fixDateFormat($_GET["start3"]);
+
+    $end1 = fixDateFormat($_GET["end1"]);
+    if (!empty($_GET['end2'])) $end2 = fixDateFormat($_GET["end2"]);
+    if (!empty($_GET['end3'])) $end3 = fixDateFormat($_GET["end3"]);
+
+    $qSetDest = "INSERT INTO tbl_205_destination (user_id, trip_id, planet_name, arrival_date, depart_date, picture_url) VALUES (4,$trip_id,'$planet_name1',$strt1,$end1,'$pic1'); ";
+
+    if (!empty($_GET['planet2'])) {
+        $qSetDest .= "INSERT INTO tbl_205_destination (user_id, trip_id, planet_name, arrival_date, depart_date, picture_url) VALUES (4,$trip_id,'$planet_name2',$strt2,$end2,'$pic2'); ";
+    }
+
+    if (!empty($_GET['planet3'])) {
+        $planet_name3 = $_GET["planet3"];
+        $qSetDest .= "INSERT INTO tbl_205_destination (user_id, trip_id, planet_name, arrival_date, depart_date, picture_url) VALUES (4,$trip_id,'$planet_name3',$strt3,$end3,'$pic3'); ";
+    }
+
+    //create a new trip too
+    $qSetDest .= "INSERT INTO tbl_205_trip (user_id) VALUES (4); ";
+    if (mysqli_multi_query($connection, $qSetDest)) {
+        echo "Huge success.<br>";
+    }
+    else {
+        echo "Error: " . $qSetDest . "<br>" . mysqli_error($connection);
+    }
 
 
-if (mysqli_query($connection, $qSetDest)) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $qSetDest . "<br>" . mysqli_error($connection);
-}
+
 mysqli_close($connection);
 
-?>
+
+
 /* to do list:
--check if dest2 and 3 are available and add them as well.
--create a new trip that will co-respond to the destination we're creating
--create 4 more pics to use randomly? i guess? depending on a digit
+V check if dest2 and 3 are available and add them as well.
+V create a new trip that will co-respond to the destination we're creating
+v create 4 more pics to use randomly? i guess? depending on a digit
+-change user id to be dynamic (get it from the form automatically)
+v change the pic to be dynamic, something about mod
+-Fix the dates, they are 0000-00-00 for some reason
 -die */
+
+
+/*i deleted from the trip tbl the day_id and the dest_id lol */
+?>
+
