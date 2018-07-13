5caef443c269dad74e345d6083300538a3cfb85f
MiriNu <miryamnudelman@gmail.com>
2018-07-13 21:50:59 +0300

    php

--- server/get-attr.php
+++ server/get-attr.php
@@ -1,25 +1,36 @@
 <?php
 	include ('db.php');
-/*$qGetAttr = "SELECT * FROM tbl_205_attraction WHERE planet='' ORDER BY rating DESC";*/
-	$qGetAttr = "SELECT * FROM tbl_205_attraction ORDER BY rating DESC";
+	/*$qGetAttr = "SELECT * FROM tbl_205_attraction WHERE planet='' ORDER BY rating DESC";*/
+
+	$qGetPlanet =  "SELECT planet_name
+						FROM tbl_205_destination plnt 
+						INNER JOIN
+						(SELECT MAX(trip_id) as max_val
+						FROM tbl_205_destination) mxvl ON plnt.trip_id = mxvl.max_val LIMIT 1";
+
+	$planetName = mysqli_fetch_array(mysqli_query($connection, $qGetPlanet))["planet_name"];
+	$qGetAttr = "SELECT * FROM tbl_205_attraction WHERE planet = '$planetName' ORDER BY rating DESC";
 	$result = mysqli_query($connection, $qGetAttr);
 	if (!$result)
 	{
+		echo "Error: " . $qGetAttr . "<br>" . mysqli_error($connection);
 		die("db query failed");
 	}
 
 	$rows = array();
 	while($r = mysqli_fetch_assoc($result)) {
-    	$rows[] = $r;
+		$rows[] = $r;
 	}
 	echo json_encode($rows);
-	
+
 	mysqli_free_result($result);
-	
+
 	mysqli_close($connection);
+
+	/* to do list:
+	-Check why not all attractions are loaded??????
+	 */
 ?>
-
-
 
 
 
--- server/set-attr.php
+++ server/set-attr.php
@@ -1,11 +1,36 @@
 <?php
-include ('db.php');
+    include ('db.php');
+
+    $get_max_attr = "SELECT MAX(attr_id) AS max_attr FROM tbl_205_attraction";
+    $num_of_attr = mysqli_fetch_array(mysqli_query($connection, $get_max_attr))["max_attr"];
+
+    $get_latest_trip = "SELECT MAX(trip_id) AS max_trip FROM tbl_205_trip";
+    $trip_id = mysqli_fetch_array(mysqli_query($connection, $get_latest_trip))["max_trip"];
+
+    $get_first_date = "";
+    $nth_date = "";
+    $nth_attr = 1;
+
+    for ($x = 2; $x <= $num_of_attr; $x++) {
+        if (!empty($_GET['attraction' . $x])) {
+            //$attr_val = $_GET['attraction' . $x];
+            if ($nth_attr%5 ==1) {
+                $qSetAttrs = "INSERT INTO tbl_205_day (trip_id, date, attr1_id) VALUES ($trip_id,$nth_date,$attr_val); ";
+            }
+            else {
+                $qSetAttrs .= "UPDATE ";
+            }
+
+            if (($x%5) == 0) {
+                /*here we need to skip to the next day and create another date oh shitz*/
+            }
+            $nth_attr++;
+        }
+    }
+    /* to do list:
+
+     */
 
 
-/* to do list:
-
- */
-
-
-/*i deleted from the trip tbl the day_id and the dest_id lol */
+    /*i deleted from the trip tbl the day_id and the dest_id lol */
 ?>
